f2aeb774fccde51eb8561bad070b5e798903b46e
nestdanchia <ndchiariello@gmail.com>
2022-04-06 15:26:16 -0300

    inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatTable } from '@angular/material/table';
+
+import * as _moment from 'moment';
+const moment = _moment;
+class Articulo {
+  constructor(public taskName: string, public taskState: string,
+    public startDate: Date, public asOfDate: Date, public updatedAt: Date) {
+  }
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +19,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pruebaAbril';
+  startDatePick = new Date(2022, 3, 1);
+  date = moment();
+
+  selDate!: string;
+  selDay !: string;
+  selMonth !: string;
+  selYear !: string;
+
+  title = "hola";
+  dataSource: any;
+  articuloselect: Articulo = new Articulo("", "", new Date(), new Date(), new Date());
+  @ViewChild(MatTable) tabla1!: MatTable<Articulo>;
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
+  columnas: string[] = ['taskName', 'taskState', 'startDate', 'asOfDate', 'updatedAt'];
+  datos: Articulo[] = [new Articulo('Deportes', 'Activo', new Date(2022, 4, 1), new Date(2022, 4, 21), new Date(2022, 4, 5)),
+  new Articulo('Cine', 'Activo', new Date(2022, 8, 1), new Date(2022, 8, 18), new Date(2022, 8, 5)),
+  new Articulo('Ajedrez', 'Activo', new Date(2022, 10, 1), new Date(2022, 10, 29), new Date(2022, 10, 14)),
+  ];
+  ngOnInit() {
+
+    this.dataSource = new MatTableDataSource<Articulo>(this.datos);
+    console.log('ngOnInit this.datos:', this.datos)
+    console.log(this.dataSource);
+    this.dataSource.sort = this.sort;
+  }
+  agregar() {
+    let nuevaTarea = new Articulo(this.articuloselect.taskName,
+      this.articuloselect.taskState, this.articuloselect.startDate,
+      this.articuloselect.asOfDate, this.articuloselect.updatedAt)
+    console.log(nuevaTarea);
+    this.datos.splice(0, 0, nuevaTarea);
+    this.dataSource = new MatTableDataSource<Articulo>(this.datos);
+    this.tabla1.renderRows();
+    this.articuloselect = new Articulo("", "", new Date("2015-03-25"), new Date(), new Date());
+  }
+  onDateChange($event: any) {
+    console.log($event.value, moment($event.value).format('YYYY-MM-DD'))
+    const formatted = moment($event.value).format('YYYY-MM-DD');
+    console.log(formatted)
+
+  }
+  addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
+    this.date = moment(event.value);
+    this.selDate = this.date.format('DD');
+    this.selDay = this.date.format('dddd');
+    this.selMonth = this.date.format('MMMM');
+    this.selYear = this.date.format('YYYY');
+  }
+
 }
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatSortModule } from '@angular/material/sort';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule, MAT_DATE_FORMATS } from '@angular/material/core';
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+
+const MY_FORMATS = {
+  parse: {
+    dateInput: 'yyyy-MM-dd',
+  },
+  display: {
+    dateInput: 'yyyy-MM-dd',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +32,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatInputModule,
+    MatSortModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    FormsModule 
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
